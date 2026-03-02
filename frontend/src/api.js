7fb75d6bb0f2d5a76956171401ad25cb809f8d3f
Gaurav Katheriya <gauravstu10@gmail.com>
2026-03-02 13:47:05 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+// In your api.js file
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 // --- SESSION MANAGEMENT ---
 // Check if they already have an ID, if not, generate a new random one
