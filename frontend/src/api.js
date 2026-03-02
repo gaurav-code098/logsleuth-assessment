ceb964213555c3d46a5377397165a7f26d2b68cc
Gaurav Katheriya <gauravstu10@gmail.com>
2026-03-02 14:00:22 +0530

    Rename BASE_URL to API_BASE_URL in api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // In your api.js file
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000"
 
 // --- SESSION MANAGEMENT ---
 // Check if they already have an ID, if not, generate a new random one
